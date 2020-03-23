3be35e911b0f71b4ad4067435153f2c9298ec55a
Vandemberg Lima <vandemberg.silva.lima@gmail.com>
2020-03-23 01:51:13 -0300

    socket.io backend <-> mobile

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, AsyncStorage, StyleSheet, Image } from 'react-native';
+import { SafeAreaView, ScrollView, AsyncStorage, StyleSheet, Image, Alert } from 'react-native';
+
+import socketio from 'socket.io-client';
 
 import logo from '../assets/logo.png';
 import SpotList from '../assets/SpotList';
 
 export default function List() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user_id => {
+      console.log(user_id);
+      const socket = socketio('http://192.168.0.100:3333', {
+        query: { user_id }
+      });
+
+      socket.on('booking_response', booking => {
+        Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJETADA'}`);
+      })
+    });
+  }, []);
 
   useEffect(() => {
     AsyncStorage.getItem('techs').then(function (storageTechs) {
